040996faa1d81b34364b2932cd8cf15a0992c3be
Olya <olapancisina1@gmail.com>
2024-03-25 15:18:50 +0100

    add Account Page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import ContentBlock from '@/components/content-block'
+import H1 from "@/components/h1";
 
 export default function page() {
-	return (
-		<div>page</div>
-	)
+  return (
+    <main>
+      <H1 className="my-8 text-white">Your Account</H1>
+      <ContentBlock className="flex h-[500px] flex-col items-center justify-center gap-3">
+        <p>Logged in as 
+					{/* {session.user.email} */}
+				</p>
+
+        {/* <SignOutBtn /> */}
+      </ContentBlock>
+    </main>
+  );
 }
