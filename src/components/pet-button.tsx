b009c39eb22d5eff272f6ab7c3dfa27c161a90f5
Olya <olapancisina1@gmail.com>
2024-03-26 11:33:31 +0100

    add pet

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,16 +1,14 @@
 import { PlusIcon } from "lucide-react";
 import { ReactNode } from "react";
+import PetForm from "./pet-form";
 import { Button } from "./ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
@@ -34,7 +32,6 @@ export default function PetButton({
   return (
     <Dialog>
       <DialogTrigger asChild>
-
         {actionType === "add" ? (
           <Button size="icon">
             <PlusIcon className="h-6 w-6" />
@@ -42,38 +39,18 @@ export default function PetButton({
         ) : (
           <Button variant="secondary">{children}</Button>
         )}
-
       </DialogTrigger>
 
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>{actionType === "add" ? "Add a new pet" : "Edit pet" }</DialogTitle>
+          <DialogTitle>
+            {actionType === "add" ? "Add a new pet" : "Edit pet"}
+          </DialogTitle>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="ownername" className="text-right">
-              Owner Name
-            </Label>
-            <Input
-              id="ownername"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">{actionType === "add" ? "Add pet" : "Save changes"}</Button>
-        </DialogFooter>
+
+        <PetForm
+          buttonText={actionType === "add" ? "Add pet" : "Save changes"}
+        />
       </DialogContent>
     </Dialog>
   );
---
+++ src/components/pet-form.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { FormEvent } from 'react'
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { usePetContext } from '@/lib/hooks'
+
+export default function PetForm({ buttonText }: { buttonText: string }) {
+
+	const {handleAddPet} = usePetContext()
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+		const newPet = {
+			name: formData.get("name") as string,
+			ownerName: formData.get("ownerName") as string,
+			imageUrl: formData.get("imageUrl") as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+			age: +(formData.get("age") as string),
+			notes: formData.get("notes") as string
+		}
+
+		handleAddPet(newPet)
+	}
+
+  return (
+    <form onSubmit={handleSubmit}  className="flex flex-col space-y-3">
+      <div className="space-y-3">
+        <div className="items-center space-y-1">
+          <Label htmlFor="name" className="text-right">
+            Name
+          </Label>
+          <Input id="name" name="name" type="text" required/>
+        </div>
+
+        <div className="items-center space-y-1">
+          <Label htmlFor="ownerName" className="text-right">
+            Owner Name
+          </Label>
+          <Input id="ownerName" name="ownerName" type="text" required/>
+        </div>
+
+        <div className="items-center space-y-1">
+          <Label htmlFor="imageUrl" className="text-right">
+            Image Url
+          </Label>
+          <Input id="imageUrl" name="imageUrl" type="text" />
+        </div>
+
+        <div className="items-center space-y-1">
+          <Label htmlFor="age" className="text-right">
+            Age
+          </Label>
+          <Input id="age" name="age" type="number" required/>
+        </div>
+
+        <div className="items-center space-y-1">
+          <Label htmlFor="notes" className="text-right">
+            Notes
+          </Label>
+          <Textarea id="notes" name="notes" rows={3} required/>
+        </div>
+      </div>
+
+      <Button type="submit" className="mt-6 self-end">
+        {buttonText}
+      </Button>
+    </form>
+  );
+}
