fae2ad2e66431a4a1527a568da0f62d19d75c948
Olya <olapancisina1@gmail.com>
2024-03-26 10:19:06 +0100

    add dialog(shadcn)

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,27 +1,80 @@
 import { PlusIcon } from "lucide-react";
 import { ReactNode } from "react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
   children?: ReactNode;
-  onClick?: () => void
+  onClick?: () => void;
 };
 
-export default function PetButton({ actionType, onClick, children }: PetButtonProps) {
-  if (actionType === "add") {
+export default function PetButton({
+  actionType,
+  onClick,
+  children,
+}: PetButtonProps) {
+  if (actionType === "checkout") {
     return (
-      <Button size="icon">
-        <PlusIcon className="h-6 w-6" />
+      <Button variant="secondary" onClick={onClick}>
+        {children}
       </Button>
     );
   }
 
-  if (actionType === "edit") {
-    return <Button variant="secondary">{children}</Button>;
-  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
 
-  if (actionType === "checkout") {
-    return <Button variant="secondary" onClick={onClick}>{children}</Button>;
-  }
+        {actionType === "add" ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{actionType === "add" ? "Add a new pet" : "Edit pet" }</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="ownername" className="text-right">
+              Owner Name
+            </Label>
+            <Input
+              id="ownername"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">{actionType === "add" ? "Add pet" : "Save changes"}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
