5223f73bacad8d1ebffeb4ea0fa98250c8e444b4
Olya <olapancisina1@gmail.com>
2024-04-03 10:32:32 +0200

    Render User Email In UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,15 +1,19 @@
-import ContentBlock from '@/components/content-block'
+import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth";
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  if(!session?.user) return { redirect: '/login' }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
       <ContentBlock className="flex h-[500px] flex-col items-center justify-center gap-3">
-        <p>Logged in as ...
-					{/* {session.user.email} */}
-				</p>
+        <p>
+          Logged in as <span className='font-semibold'>{session.user.email}</span>
+        </p>
 
         {/* <SignOutBtn /> */}
       </ContentBlock>
