1d1fe781536037f1b61792231717efbb66bfc794
Olya <olapancisina1@gmail.com>
2024-04-03 10:17:56 +0200

    Finish Login & Protect App

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,6 @@ import NextAuth, { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prisma from "./db";
 
-
 const config = {
   pages: {
     signIn: "/login",
@@ -39,12 +38,19 @@ const config = {
     }),
   ],
   callbacks: {
-    authorized: ({ request }) => {
+    authorized: ({ auth, request }) => {
+      const isLoggedIn = Boolean(auth?.user);
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
 
-      if (isTryingToAccessApp) {
+      if (!isLoggedIn && isTryingToAccessApp) {
         return false;
-      } else {
+      }
+
+      if (isLoggedIn && isTryingToAccessApp) {
+        return true;
+      }
+
+      if (!isTryingToAccessApp) {
         return true;
       }
     },
